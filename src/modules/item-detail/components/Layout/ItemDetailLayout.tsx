2ab6d413b9d092ccc233fef5ec8e830dd38649bb
Gabriel Cardozo <gcardozo@Gabriels-MacBook-Pro.local>
2025-03-31 00:27:33 -0300

    style: improves details product page

--- src/modules/item-detail/components/Layout/ItemDetailLayout.tsx
+++ src/modules/item-detail/components/Layout/ItemDetailLayout.tsx
@@ -2,14 +2,23 @@ import Breadcrumb from "../Breadcrumb/Breadcrumb";
 import ProductInfo from "../ProductInfo/ProductInfo";
 import ImageProduct from "../ImageProduct/ImageProduct";
 import ProductDescription from "../ProductDescription/ProductDescription";
+import styles from "./ItemDetailLayout.module.scss";
 
 const ItemDetailLayout = () => {
   return (
     <>
       <Breadcrumb />
-      <ImageProduct />
-      <ProductInfo />
-      <ProductDescription />
+      <div className={styles.container}>
+        <div className={styles.image_area}>
+          <ImageProduct />
+        </div>
+        <div className={styles.info_area}>
+          <ProductInfo />
+        </div>
+        <div className={styles.description_area}>
+          <ProductDescription />
+        </div>
+      </div>
     </>
   );
 };
