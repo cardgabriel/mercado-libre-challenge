e028cba19f14013e5eecc75e4cb7cfccddace11c
Gabriel Cardozo <gcardozo@Gabriels-MacBook-Pro.local>
2025-04-02 19:27:24 -0300

    feat: consume get product detail

--- client/src/modules/common/urls.ts
+++ client/src/modules/common/urls.ts
@@ -3,4 +3,5 @@ const API_BASE_URL = "http://localhost:3000/api";
 export const API_URLS = {
   GET_PRODUCTS: (query: string) =>
     `${API_BASE_URL}/items?q=${encodeURIComponent(query)}`,
+  GET_PRODUCT_DETAIL: (id: string) => `${API_BASE_URL}/items/${id}`,
 };
--- client/src/modules/product-detail/components/ProductDetailPage/ProductDetailPage.tsx
+++ client/src/modules/product-detail/components/ProductDetailPage/ProductDetailPage.tsx
@@ -3,8 +3,25 @@ import ImageProduct from "../ImageProduct/ImageProduct";
 import ProductDescription from "../ProductDescription/ProductDescription";
 import styles from "./ProductDetailPage.module.scss";
 import Breadcrumb from "@/modules/common/components/Breadcrumb/Breadcrumb";
+import { useParams } from "react-router-dom";
+import useQuery from "@/modules/common/hooks/useQuery";
+import { API_URLS } from "@/modules/common/urls";
+import NotFound from "@/modules/common/components/NotFound/NotFound";
+import Loading from "@/modules/common/components/Loading/Loading";
 
 const ProductDetailLayout = () => {
+  const { id } = useParams();
+
+  const {
+    data: productData,
+    loading,
+    error,
+  } = useQuery(API_URLS.GET_PRODUCT_DETAIL(id!));
+
+  if (loading) return <Loading />;
+  if (error) return <NotFound />;
+  console.log(productData);
+
   return (
     <>
       <Breadcrumb />
