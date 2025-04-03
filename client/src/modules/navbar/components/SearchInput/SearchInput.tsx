ff1ebcd3e5868fc51478dbb7a71505c25dc5f377
Gabriel Cardozo <gcardozo@Gabriels-MacBook-Pro.local>
2025-04-02 21:51:23 -0300

    feat: add searchbar

--- client/src/modules/navbar/components/SearchInput/SearchInput.tsx
+++ client/src/modules/navbar/components/SearchInput/SearchInput.tsx
@@ -1,12 +1,18 @@
 import { useState, KeyboardEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./SearchInput.module.scss";
 import SearchIcon from "@/assets/icons/ic_search.webp";
+import { ROUTES } from "@/modules/common/urls";
 
 const SearchInput = () => {
   const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   const handleSearch = () => {
-    console.log("buscando...");
+    if (searchValue.trim()) {
+      const encodedSearch = encodeURIComponent(searchValue.trim());
+      navigate(ROUTES.PRODUCT_LIST(encodedSearch));
+    }
   };
 
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
@@ -19,22 +25,18 @@ const SearchInput = () => {
     <div className={styles.searchContainer}>
       <input
         type="text"
-        placeholder= "Nunca dejes de buscar"
+        placeholder="Nunca dejes de buscar"
         className={styles.searchInput}
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
         onKeyDown={handleKeyDown}
       />
-      <button 
+      <button
         className={styles.searchButton}
         onClick={handleSearch}
         aria-label="Buscar"
       >
-        <img 
-          src={SearchIcon} 
-          alt="Buscar" 
-          className={styles.searchIcon}
-        />
+        <img src={SearchIcon} alt="Buscar" className={styles.searchIcon} />
       </button>
     </div>
   );
