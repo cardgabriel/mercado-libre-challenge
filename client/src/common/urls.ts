85aba3306d3260cd523ae460ae839acbdd4ed9cf
Gabriel Cardozo <gcardozo@Gabriels-MacBook-Pro.local>
2025-04-03 22:31:02 -0300

    feat: deploy config

--- client/src/common/urls.ts
+++ client/src/common/urls.ts
@@ -1,9 +1,9 @@
-const API_BASE_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 export const API_URLS = {
   GET_PRODUCTS: (query: string) =>
-    `${API_BASE_URL}/items?q=${encodeURIComponent(query)}`,
-  GET_PRODUCT_DETAIL: (id: string) => `${API_BASE_URL}/items/${id}`,
+    `${API_URL}/items?q=${encodeURIComponent(query)}`,
+  GET_PRODUCT_DETAIL: (id: string) => `${API_URL}/items/${id}`,
 };
 
 export const ROUTES = {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { ProductController } from "./product.controller";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
